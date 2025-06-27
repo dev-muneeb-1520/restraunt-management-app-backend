406dda230ef31673c6180b6b926fbea8933db883
Muhammad Muneeb Saleem <dev.themuneeb@gmail.com>
2025-06-27 18:21:24 +0500

    configure swagger docs and prisma client

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Restruant Management App API')
+    .setDescription(
+      "This is the backend service for a Restaurant Management System developed using NestJS, Prisma ORM, and PostgreSQL. It provides secure and modular REST APIs to manage various aspects of a restaurant's operations",
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
